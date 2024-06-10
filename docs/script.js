9efcac6b9f65444b77cd850978081bec939511f9
martin852147 <cenigamartin@gmail.comm>
2024-06-10 17:48:07 +0200

    Loading OFF

--- docs/script.js
+++ docs/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
     document.getElementById("loading-screen").style.display = "none";
     document.getElementById("content").style.display = "block";
-  }, 2000);
+  }, 0);
 });
 
 function updateClock() {
